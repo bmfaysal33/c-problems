282c0cf11af8706031d07f8bbab0c98687884e3f
bmfaysal33 <bmfaysal33@gmail.com>
2026-02-08 09:43:11 +0600

    problem 1

--- c-fourty.c
+++ c-fourty.c
@@ -1,7 +1,20 @@
+
+//  1. Check whether a number is even or odd.
+
+
+
 #include <stdio.h>
 
 int main(){
 
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num%2==0) printf("Even \n");
+    else printf("Odd \n");
+    
 
     return 0;
 }
